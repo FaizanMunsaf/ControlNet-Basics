d9ba3e8cce264ae623be9fd0d98ef2e4af4ba4c7
FaizanMunsaf <faizanmunsaf@gmail.com>
2025-10-18 01:14:17 +0500

    flux Canny implementation, for understanding the concept of control net

---
+++ text-to-image/canny_image.py
@@ -0,0 +1,21 @@
+
+import cv2
+import numpy as np
+from PIL import Image
+from diffusers.utils import load_image
+
+original_image = load_image(
+    "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/non-enhanced-prompt.png"
+)
+
+image = np.array(original_image)
+
+low_threshold = 100
+high_threshold = 200
+
+image = cv2.Canny(image, low_threshold, high_threshold)
+image = image[:, :, None]
+image = np.concatenate([image, image, image], axis=2)
+canny_image = Image.fromarray(image)
+canny_image.show()
+canny_image.save("test-images/canny_image.png")
